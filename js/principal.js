cde082f77342ff1b400d4222e79079d0ad3dfcf8
BrunaLopesGois <bruna-chan@live.com>
2021-01-26 15:18:02 -0300

    calculating and validating imc

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,14 @@
 var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector(".paciente");
+var peso = paciente.querySelector(".info-peso").textContent;
+var altura = paciente.querySelector(".info-altura").textContent;
+var imc = peso / (Math.pow(altura, 2));
+
+if (peso <= 0 || peso >= 1000 || altura <= 0 || altura >= 3.00) {
+    paciente.querySelector(".info-imc").textContent = "Peso ou altura inválido(s)";
+} else {
+    paciente.querySelector(".info-imc").textContent = imc;
+}
