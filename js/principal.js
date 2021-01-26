020cbd9649573904436a57a3f3c64b2433450887
BrunaLopesGois <bruna-chan@live.com>
2021-01-26 18:29:11 -0300

    calculating the imc for all patients

--- js/principal.js
+++ js/principal.js
@@ -2,13 +2,16 @@ var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector(".paciente");
-var peso = paciente.querySelector(".info-peso").textContent;
-var altura = paciente.querySelector(".info-altura").textContent;
-var imc = peso / (Math.pow(altura, 2));
+var pacientes = document.querySelectorAll(".paciente");
+pacientes.forEach(paciente => {
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+    var imc = (peso / (Math.pow(altura, 2))).toFixed(2);
 
-if (peso <= 0 || peso >= 1000 || altura <= 0 || altura >= 3.00) {
-    paciente.querySelector(".info-imc").textContent = "Peso ou altura inválido(s)";
-} else {
-    paciente.querySelector(".info-imc").textContent = imc;
-}
+    if (peso <= 0 || peso >= 1000 || altura <= 0 || altura >= 3.00) {
+        paciente.querySelector(".info-imc").textContent = "Peso ou altura inválido(s)";
+        paciente.classList.add("paciente-invalido");
+    } else {
+        paciente.querySelector(".info-imc").textContent = imc;
+    }
+});
